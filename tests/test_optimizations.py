9ec504103d6204964cba8430d33943c6d48eeb11
Krmsh1n5 <Dima7715@icloud.com>
2025-03-06 21:32:55 +0400

    Adjusted file paths in the code

--- tests/test_optimizations.py
+++ tests/test_optimizations.py
@@ -10,7 +10,7 @@ import unittest
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '/home/krmsh1n5/Desktop/Python/Projects/Levenshtein-FuzzySearch/src/')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/')))
 
 from optimizations.bk_tree import BKTree
 from optimizations.parallel_processing import parallel_fuzzy_search
